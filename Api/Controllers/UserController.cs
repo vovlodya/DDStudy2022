22f4ec0b994289032c34ce6b40f05b1592cfbc54
vovan <vovan@VOVLODYA>
2022-10-27 17:43:57 +0300

    Добавьте файлы проекта.

---
+++ Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Api.Models;
+using AutoMapper;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper.QueryableExtensions;
+using Api.Services;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task CreateUser(CreateUserModel model)=> await _userService.CreateUser(model);
+        [HttpGet]
+        public async Task<List<UserModel>> GetUsers() => await _userService.GetUsers();
+        
+    }
+}
---
+++ Api/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Common;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Api
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Models.CreateUserModel, DAL.Entities.User>()
+                .ForMember(d => d.Id, m => m.MapFrom(s => Guid.NewGuid()))
+                .ForMember(d => d.PasswordHash, m => m.MapFrom(s => HashHelper.GetHash(s.Password)))
+                .ForMember(d => d.BirthDate, m=> m.MapFrom(s=>s.BirthDate.UtcDateTime))
+                ;
+            CreateMap<DAL.Entities.User, Models.UserModel>();
+        }
+    }
+}
